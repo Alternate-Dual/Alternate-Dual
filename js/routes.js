5d96573dc2a3500509ff146863df2fc8a24e500c
L1L3V4 <eva.s284913@cesurformacion.com>
2023-02-01 13:03:08 +0100

    Server y routes

--- js/routes.js
+++ js/routes.js
@@ -1,8 +1,30 @@
-const express = requiere('express')
+const express = require('express')
 const routes = express.Router()
 
-routes.get("/menunutricionista", function(request, response){ //Renderizado de páginas mustache (uno por plantilla)
+routes.get('/', (req, res)=>{
+    res.send('raiz de routes')
+})
+
+routes.get("/menunutricionista", function(request, response){ 
     response.render('menu_nutricionista');
 })
+
+routes.get("/menupaciente", function(request, response){
+    response.render('menu_paciente');
+})
+
+routes.get("/login", function(request, response){
+    response.render('login');
+})
+
+
+//------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+routes.get("/datoscontacto", function(request, response){ // EJEMPLO **Renderizado de páginas mustache (uno por plantilla), "127.0.0.1:3000/datoscontacto"**
+    response.render('datos_contacto_nutri', { tel: 'Hey', email: 'Hello there!'});
+})
+
+
+//------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 module.exports = routes
--- js/server.js
+++ js/server.js
@@ -4,15 +4,7 @@ let bodyParser = require('body-parser');
 const res = require('express/lib/response');
 let app = express();
 
-const mysql = require('mysql')  //Conexion BDD
-const myconn = require('express-myconnection')
-const dbOptions = {
-    host: 'localhost',
-    port: 3306,
-    user: 'root',
-    password: '',
-    database: 'nutrifit'
-}
+const routes = require('./routes')
 
 app.set('views', `${__dirname}/views`);
 app.set('view engine', 'mustache');
@@ -20,7 +12,16 @@ app.engine('mustache', mustacheExpress());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-const routes = require('./routes')
+//Conexion BDD---------------------------------
+const mysql = require('mysql')  
+const myconn = require('express-myconnection')
+const dbOptions = {
+    host: 'localhost',
+    port: 3307,
+    user: 'root',
+    password: '',
+    database: 'nutrifit'
+}
 
 // Middlewares (conector)----------------------
 app.use(myconn(mysql, dbOptions, 'single'))
@@ -34,23 +35,9 @@ app.listen(3000, function () {
 app.get('/', (req, res)=>{
     res.send('Bienvenido a mi API')
 })
-
-app.use('/menunutricionista', routes)
-
+app.use('/api', routes) //Llama a las rutas de las plantillas mustache
 
 app.get("/echo/:a", function(request, response){ //Desbuggear plantillas (Ver si nos coge los parametros o no)
     console.log(request)
     response.send(request.params.a)
 });
-
-app.get("/menupaciente", function(request, response){ //Renderizado de páginas mustache (uno por plantilla)
-    response.render('menu_paciente');
-})
-
-app.get("/login", function(request, response){ //Renderizado de páginas mustache (uno por plantilla)
-    response.render('login');
-})
-
-app.get("/datoscontacto", function(request, response){ //Renderizado de páginas mustache (uno por plantilla), "127.0.0.1:3000/datoscontacto" 
-    response.render('datos_contacto_nutri', { tel: 'Hey', email: 'Hello there!'}); //Ejemplo
-})
