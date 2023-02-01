ee233a9585708b2a422e8c7a85e96e131746869a
L1L3V4 <eva.s284913@cesurformacion.com>
2023-02-01 23:55:36 +0100

    haciendo un poco lo que puedo

--- js/routes.js
+++ js/routes.js
@@ -5,19 +5,6 @@ routes.get('/', (req, res)=>{
     res.send('raiz de routes')
 })
 
-routes.get("/menunutricionista", function(request, response){ 
-    response.render('menu_nutricionista');
-})
-
-routes.get("/menupaciente", function(request, response){
-    response.render('menu_paciente');
-})
-
-routes.get("/login", function(request, response){
-    response.render('login');
-})
-
-
 //------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 routes.get("/datoscontacto", function(request, response){ // EJEMPLO **Renderizado de páginas mustache (uno por plantilla), "127.0.0.1:3000/datoscontacto"**
--- js/server.js
+++ js/server.js
@@ -1,10 +1,12 @@
 let express = require('express');
 let mustacheExpress = require('mustache-express');
 let bodyParser = require('body-parser');
-const res = require('express/lib/response');
 let app = express();
 
-const routes = require('./routes')
+const res = require('express/lib/response');
+const routes = require('./routes');
+const { engine } = require('express-handlebars');
+const session = require('express-session');
 
 app.set('views', `${__dirname}/views`);
 app.set('view engine', 'mustache');
@@ -12,7 +14,7 @@ app.engine('mustache', mustacheExpress());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-//Conexion BDD---------------------------------
+//Conexion BDD y sesiones---------------------------------
 const mysql = require('mysql')  
 const myconn = require('express-myconnection')
 const dbOptions = {
@@ -22,6 +24,12 @@ const dbOptions = {
     password: '',
     database: 'nutrifit'
 }
+
+app.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+}));
 
 // Middlewares (conector)----------------------
 app.use(myconn(mysql, dbOptions, 'single'))
@@ -36,6 +44,54 @@ app.get('/', (req, res)=>{
     res.send('Bienvenido a mi API')
 })
 app.use('/api', routes) //Llama a las rutas de las plantillas mustache
+
+app.get("/menunutricionista", function(request, response){ 
+    response.render('menu_nutricionista');
+})
+
+app.get("/menupaciente", function(request, response){
+    response.render('menu_paciente');
+})
+
+app.get("/menuadmin", function(request, response){
+    response.render('menu_admin');
+})
+
+app.get("/login", function(request, response){
+    response.render('login');
+})
+
+app.get("/registro_nutricionista", function(request, response){
+    response.render('registro_nutricionista');
+})
+
+app.get("/index_nutricionista", function(request, response){
+    response.render('index_nutricionista');
+})
+
+app.get("/index_paciente", function(request, response){
+    response.render('index_paciente');
+})
+
+app.get("/index_admin", function(request, response){
+    response.render('index_admin');
+})
+
+app.get("/perfil_nutricionista", function(request, response){
+    response.render('perfil_nutricionista');
+})
+
+app.get("/perfil_paciente", function(request, response){
+    response.render('perfil_paciente');
+})
+
+app.get("/administrar_nutricionistas", function(request, response){
+    response.render('administrar_nutricionistas');
+})
+
+app.get("/script", function(request, response){
+    response.render('script');
+})
 
 app.get("/echo/:a", function(request, response){ //Desbuggear plantillas (Ver si nos coge los parametros o no)
     console.log(request)
